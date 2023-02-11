71635a8ffb86b0db992e5aeaad60727dc09a3edf
illusion <78088291+illusionhuan@users.noreply.github.com>
2023-02-11 06:48:42 +0000

    更新内容

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -3,34 +3,41 @@
         <h3 class="login_title">
             登录界面
         </h3>
+        
         <el-button class="register_button" type="text" style="width:70%;" @click="register()">点此注册</el-button>
-        <el-form-item prop="login_name">
-            <el-input placeholder="账号"  v-model="login_Form.login_name" prefix-icon="el-icon-user" autocomplete="on" clearable></el-input>
+        
+        <el-form-item prop="username">
+        
+            <el-input placeholder="账号"  v-model="login_Form.username" prefix-icon="el-icon-user" autocomplete="off" clearable></el-input>
+        
         </el-form-item>
-        <el-form-item prop="login_password">
-            <el-input placeholder="密码" type="password" v-model="login_Form.login_password" prefix-icon="el-icon-lock" autocomplete="on" show-password clearable></el-input>
+        
+        <el-form-item prop="password">
+        
+            <el-input placeholder="密码" type="password" v-model="login_Form.password" prefix-icon="el-icon-lock" autocomplete="off" show-password clearable></el-input>
+        
         </el-form-item>
-            <el-button type="primary" style="width:50%;border:none" @click="login()">登录</el-button>
+        
+        <el-button type="primary" style="width:50%;border:none" @click="login">登录</el-button>
+        <!-- <el-button type="info" style="width:50%;border:none" @click="resetLoginForm">重置</el-button> -->
     </el-form>
 </template>
 <script>
-import Mock from 'mockjs'
-import Cookie from 'js-cookie'
 export default {
     name: "Login-Name",
      data(){
         return{
             formData: {},
             login_Form: {
-                login_name:'',
-                login_password:'',
+                username:'',
+                password:'',
             },
             Rules: {
-                login_name:[
+                username:[
                     { required: true, message: "用户名为必填项", trigger: 'blur'},
                     { min: 3, max: 12, message: '长度在3到12个字符', trigger: 'blur'}
                 ],
-                login_password:[
+                password:[
                     { required: true, message: "密码为必填项", trigger: 'blur'},
                     { min: 6, max: 10, message: "密码长度在6~10个字符之间", trigger: 'blur'}
                 ]
@@ -45,13 +52,15 @@ export default {
         },
         login() {
             // token信息
-            const token = Mock.Random.guid();
-            Cookie.set('token',token);
-            this.$router.push('/home');
+            console.log('用户名:',this.login_Form.username);
+            console.log('密码:',this.login_Form.password);
         },
+        // resetLoginForm() {
+        //     this.$refs.loginFormRef.resetFields();  
+        // },
     //   login() {
-    //       var ln = this.login_name;
-    //       var lp = this.login_password;
+    //       var ln = this.username;
+    //       var lp = this.password;
 
     //       this.formData ={};
 
@@ -60,16 +69,16 @@ export default {
     //       }else if(lp==''){
     //           this.$message.error("请填写密码!");
     //       }else {
-    //           this.formData.login_name = ln;
-    //           this.formData.login_password = lp;
+    //           this.formData.username = ln;
+    //           this.formData.password = lp;
     //           console.log(this.formData);
     //           axios.get("http://localhost:8080/SpringMVC_ssm/users/"+ln).then((res)=>{
     //               if(res.data.data == null) {
     //                   this.$message.error("账号不存在,请注册");
-    //               }else if(res.data.data.login_password == lp){
+    //               }else if(res.data.data.password == lp){
     //                   this.$message.success("登录成功");
     //                   window.location.href = 'books.html';
-    //               }else if(res.data.data.login_password != lp) {
+    //               }else if(res.data.data.password != lp) {
     //                   this.$message.error("密码不正确,请重新输入");
     //               }
     //           });
